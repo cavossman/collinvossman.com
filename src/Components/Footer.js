4f014194d695d7d0a77377976f6e4ee212aba398
Collin Vossman <collin@builderdesigns.com>
2018-08-14 02:05:23 -0500

    update linkedin link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -12,7 +12,7 @@ class Header extends Component {
       <footer className="footer">
         <div className="container">
           <div className="social-links">
-            <a href="https://www.linkedin.com/in/collin-vossman-b5a29969/" rel="noopener noreferrer" target="_blank"><img src={ linkedIn } alt="LinkedIn Profile" /></a>
+            <a href="https://www.linkedin.com/in/collin-vossman/" rel="noopener noreferrer" target="_blank"><img src={ linkedIn } alt="LinkedIn Profile" /></a>
             <a href="https://github.com/cavossman" rel="noopener noreferrer" target="_blank"><img src={ github } alt="GitHub Profile" /></a>
           </div>
           <span>&#169; { new Date().getFullYear() }</span>
