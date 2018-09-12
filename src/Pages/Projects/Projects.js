ef7d29a52c39aaf451f6693810b4bfd2debb0712
Collin Vossman <cavossman@gmail.com>
2018-09-12 00:04:10 -0500

    add allaboutjv.com to projects

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -7,6 +7,8 @@ import ProjectCard from '../../Components/ProjectCard/ProjectCard';
 import mc from '../../images/projects/micro-cloud/interface-buildings.png';
 import mm_map from '../../images/projects/micro-map/campus_pins.png';
 import mm_building from '../../images/projects/micro-map/building_view.png';
+import allaboutjv_homepage from '../../images/projects/allaboutjv.com/homepage.PNG';
+import allaboutjv_photos from '../../images/projects/allaboutjv.com/photos.PNG';
 
 import './Projects.css';
 
@@ -19,12 +21,9 @@ class Projects extends Component {
         <div className="current container">
           <span className="section-title">Current Projects</span><br />
           <span className="disclaimer">*More information and progress screenshots to come.</span>
+          <ProjectCard title="allaboutjv.com" featuredImages={[allaboutjv_homepage, allaboutjv_photos]} description="Website for my wife to display some of her photography, writing and music." />
           <ProjectCard title="Micro Cloud" featuredImages={[mc]} description="Content Management System (CMS) for Micro Map applications.  API utilizes Node, Express and MongoDB.  Interface utilizes React framework." />
           <ProjectCard title="Micro Map" featuredImages={[mm_map, mm_building]} description="Mobile application to help students navigate a new unfamiliar campus.  Currently Android only.  Writen in Android Studio using Java and XML." />
-        </div>
-        <div className="upcoming container">
-          <span className="section-title">Upcoming Project Ideas</span>
-          <ProjectCard title="OSRS Grand Exchange Interface" description="Interface to display information from api for Old School RuneScape." />
         </div>
       </div>
     );
